1582d360f2176d160e7d1de1c3462a96bf008559
John Matsikas <john.matsikas@gmail.com>
2020-11-25 16:27:21 -0700

    cleared openzeppelin error

--- contracts/DisburseToken.sol
+++ contracts/DisburseToken.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 //pragma solidity >=0.4.22 <0.8.0;
-pragma solidity ^0.6.0;
+pragma solidity >=0.6.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DisburseToken is ERC20 {
 
