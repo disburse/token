ee431dbce32d056ecf06529ebfde541e8daed7d2
John Matsikas <john.matsikas@gmail.com>
2021-01-28 14:59:59 -0700

    testing trasfer to contract with fallback

---
+++ contracts/Contract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+//pragma solidity >=0.4.22 <0.8.0;
+pragma solidity >=0.6.0;
+
+contract Contract {
+
+    // fallback function
+    fallback() external payable { }
+
+}
--- transfer.js
+++ transfer.js
@@ -1,8 +1,12 @@
 const Web3 = require("web3");
 
 const TOKEN_JSON = require('./build/contracts/DisburseToken.json');
+const CONTRACT_JSON = require('./build/contracts/Contract.json');
+
 let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 let toAddress = "0x58FaAbD4416698041526Be00e5160bB6a28Ab07b";
+let tokenAddress = TOKEN_JSON.networks[5777].address;
+let contractAddress = CONTRACT_JSON.networks[5777].address;
 
 var getNetwork = async () => {
   return await web3.eth.net.getId();
@@ -18,27 +22,30 @@ var accountTransfer = async () => {
   let id = await getNetwork();
   let mintAccount = await getAccount();
   console.log('Network: ' + id)
-  console.log('Contract Address: ' + TOKEN_JSON.networks[5777].address);
+  console.log('Token Address: ' + tokenAddress);
   console.log('Mint account: ' + mintAccount);
+  console.log();
 
-  let token = new web3.eth.Contract(TOKEN_JSON.abi, TOKEN_JSON.networks[5777].address);
+  let token = new web3.eth.Contract(TOKEN_JSON.abi, tokenAddress);
 
   let balance = await token.methods.balanceOf(mintAccount).call();
   console.log('Mint balance: ' + balance);
 
   balance = await token.methods.balanceOf(toAddress).call();
-  console.log('Wallet balance: ' + balance);
+  console.log('Initial wallet balance: ' + balance);
 
   let value = '1000000000000000000'
   let result = await token.methods.transfer(toAddress, value).send({from: mintAccount});
   //console.log(result);
 
+  console.log();
   balance = await token.methods.balanceOf(mintAccount).call();
-  console.log('New Mint balance: ' + balance);
+  console.log('New mint balance: ' + balance);
 
   balance = await token.methods.balanceOf(toAddress).call();
   console.log('New wallet balance: ' + balance);
 
+  console.log();
   console.log('Account transfer complete');
 }
 
@@ -46,7 +53,33 @@ var contractTransfer = async () => {
 
   // TODO: Create a solidity test contract that will receive ERC20 tokens
 
+  console.log('Contract: ' + contractAddress);
+  console.log();
+
+  let mintAccount = await getAccount();
+
+  let token = new web3.eth.Contract(TOKEN_JSON.abi, TOKEN_JSON.networks[5777].address);
+
+  let balance = await token.methods.balanceOf(mintAccount).call();
+  console.log('Mint balance: ' + balance);
+
+  balance = await token.methods.balanceOf(contractAddress).call();
+  console.log('Initial contract balance: ' + balance);
+
+  let value = '1000000000000000000'
+  let result = await token.methods.transfer(contractAddress, value).send({from: mintAccount});
+
+  console.log();
+  balance = await token.methods.balanceOf(mintAccount).call();
+  console.log('New mint balance: ' + balance);
+
+  balance = await token.methods.balanceOf(contractAddress).call();
+  console.log('New contract balance: ' + balance);
+
+  console.log();
+  console.log('Contract transfer complete');
+
 }
 
 accountTransfer();
-contractTransfer();
+//contractTransfer();
